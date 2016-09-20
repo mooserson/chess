cc485d3cbc2470facfec96c831d3a408515d794e
Francisco Delgado <fraencisco.delgado@gmail.com>
2016-09-20 08:08:01 -0700

    add symbols hash to piece class

--- piece.rb
+++ piece.rb
@@ -1,4 +1,24 @@
 class Piece
+
+  SYMBOLS = {
+    #white symbols
+    :white_king = "\u2654"
+    :white_queen = "\u2655"
+    :white_rook = "\u2656"
+    :white_bishop = "\u2657"
+    :white_knight = "\u2658"
+    :white_pawn = "\u2659"
+    #black symbols
+    :black_king = "\u265A"
+    :black_queen = "\u265B"
+    :black_rook = "\u265c"
+    :black_bishop = "\u265d"
+    :black_knight = "\u265e"
+    :black_pawn = "\u265f"
+
+
+  }
+
   attr_reader :value
   def initialize
     @value = "X"
